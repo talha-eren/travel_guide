c863333172df3544a26c4ef8e6798518cafb533b
SemsettinArslan <semsettinarslan1983@gmail.com>
2025-01-10 20:42:04 +0300

    Düzenleme

--- frontyeni/assets/js/config.js
+++
@@ -1,5 +0,0 @@
-// API Konfigürasyonları
-const GOOGLE_MAPS_API_KEY = 'AIzaSyA5gAWEVxOK7MEHGIz8Gq55WZzr3wEZGpM';  // Google Maps API anahtarınız
-const API_URL = 'http://localhost:5000/api';  // Backend API URL'si
-
-// Diğer konfigürasyonlar buraya eklenebilir 
